5ac6d4bf57b232ab0bb2415c5b364ac3cc23b46a
KEVIN DELASSUS - STUDENT <G00270791@gmit.ie>
2017-11-15 10:57:28 +0000

    Part 3 Complete Comments Need

--- RegularExpressions.go
+++ RegularExpressions.go
@@ -14,14 +14,18 @@ func main(){
 	rand.Seed(time.Now().UTC().UnixNano())
 	
 
-	var userInputs [5]string
+	var userInputs [9]string
 	userInputs[0] = "People say I look like both my mother and father."
 	userInputs[1] = "Father was a teacher."
 	userInputs[2] = "I was my father's favourite."
 	userInputs[3] = "I'm looking forward to the weekend."
 	userInputs[4] = "My grandfather was French!"
+	userInputs[5] = "I am Happy."
+	userInputs[6] = "I am not happy with you responses."
+	userInputs[7] = "I am not sure that you understand the effect that your questions are having on me."
+	userInputs[8] = "I am supposed to just take what you're saying at face value?"
 
-	for i:=0;i<5;i++{
+	for i:=0;i<9;i++{
 		response := ElizaResponce(userInputs[i])
 		fmt.Println("Eliza: ",response)
 		fmt.Println()
@@ -39,6 +43,12 @@ func ElizaResponce(input string) string{
 	if matched, _ := regexp.MatchString(`(?i).*\bfather\b.*`, input); matched {
 		return "Why don't you tell me more about your Father?"
 	}
+
+	re := regexp.MustCompile("I am ([^.!?]*)[.!?]?")
+	if re.MatchString(input){
+		return re.ReplaceAllString(input,"How do you know you are $1?")
+	}
+	
 
 	ranNum := rand.Intn(3)
 
