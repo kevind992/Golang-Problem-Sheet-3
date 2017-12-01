90db9cd2e0631615ba5855adf35d8f3a6a1711d6
Kevin Delassus <g00270791@gmit.ie>
2017-12-01 09:38:29 +0000

    Question 4 Complete

--- RegularExpressions.go
+++ RegularExpressions.go
@@ -6,6 +6,7 @@ import(
 	"math/rand"
 	"time"
 	"regexp"
+	"strings"
 
 )
 
@@ -20,7 +21,7 @@ func main(){
 	userInputs[3] = "I'm looking forward to the weekend."
 	userInputs[4] = "My grandfather was French!"
 	userInputs[5] = "I am Happy."
-	userInputs[6] = "I am not happy with you responses."
+	userInputs[6] = "I'm not happy with you responses."
 	userInputs[7] = "I am not sure that you understand the effect that your questions are having on me."
 	userInputs[8] = "I am supposed to just take what you're saying at face value?"
 
@@ -32,12 +33,12 @@ func main(){
 }
 func ElizaResponce(input string) string{
 	
-	fmt.Println("User: ",input)
-	
 	var response [3]string
 	response[0] = "I'm not sure what you're trying to say. Could you explain it to me?"
 	response[1] = "How does that make you feel?"
 	response[2] = "Why do you say that?"
+
+	fmt.Println("User: ",input)
 
 	if matched, _ := regexp.MatchString(`(?i).*\bfather\b.*`, input); matched {
 		return "Why don't you tell me more about your Father?"
@@ -47,6 +48,39 @@ func ElizaResponce(input string) string{
 	if re1.MatchString(input){
 		return re1.ReplaceAllString(input,"How do you know you are $1?")
 	}
+
+	re := regexp.MustCompile(`(?i)i(?:'|\sa)?m (.*)`)
+	deleteIAm := re.FindStringSubmatch(input)
+
+	// List the reflections.
+	reflections := [][]string{
+		{"your", "my"},
+		{"you’re", "i am"},
+		{"I", "you"},
+		{"you", "I"},
+		{"me", "you"},
+	}
+
+	if len(deleteIAm) > 0 {
+
+		splitInput := strings.Split(deleteIAm[1], " ")
+
+		// Loop through each word, reflecting it if there's a match.
+		for i, check := range splitInput {
+			for _, reflection := range reflections {
+				if matched, _ := regexp.MatchString(reflection[0], check); matched {
+					splitInput[i] = reflection[1]
+					break
+				}
+			}
+		}
+
+		// Put the string back together.
+		joinString := strings.Join(splitInput, " ")
+
+		return fmt.Sprintf("How do you know you are %s?", joinString)
+	}
+
 	ranNum := rand.Intn(3)
 
 	return response[ranNum]
